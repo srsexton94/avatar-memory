03ddfd2520f22363fa75ec7f789e2704f0fd0202
srsexton94 <srsexton94@gmail.com>
2025-02-15 19:12:14 -0500

    make responsive, variable board layout

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+function cloneCard(boardSize = 4) {
+  const numberOfCopies = boardSize ** 2 - 1;
+  const cardEl = document.querySelector(".card");
+  const boardEl = document.querySelector(".board");
+
+  if (boardSize > 8) {
+    cardEl.style.height = "5vw";
+    cardEl.style.width = "5vw";
+  }
+
+  for (let i = 0; i < numberOfCopies; i++) {
+    const clonedElement = cardEl.cloneNode(true);
+    boardEl.appendChild(clonedElement);
+  }
+
+  boardEl.style["grid-template-columns"] = `repeat(${boardSize}, 1fr)`;
+}
+
+cloneCard(14); // 4, 6, 8, 10, 12, 14
