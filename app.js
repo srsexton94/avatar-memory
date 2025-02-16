3a18da4a1a89c7610b93bf8afbb73aeb30a6ac18
srsexton94 <srsexton94@gmail.com>
2025-02-16 11:11:42 -0500

    shuffle and fill game board

--- app.js
+++ app.js
@@ -1,9 +1,63 @@
-function cloneCard(boardSize = 4) {
+let images = [
+  "aang1.webp",
+  "aang2.jpg",
+  "aang3.jpg",
+  "appa1.webp",
+  "azula1.jpeg",
+  "cabbages1.avif",
+  "elements1.jpg",
+  "flying1.webp",
+  "gang1.jpg",
+  "gang2.webp",
+  "gyatso.webp",
+  "hama1.webp",
+  "iroh1.jpeg",
+  "iroh2.webp",
+  "iroh3.png",
+  "jeongjeong1.webp",
+  "katara1.jpg",
+  "katara2.webp",
+  "momo1.webp",
+  "ozai1.webp",
+  "sokka1.webp",
+  "sokka2.png",
+  "toph1.jpg",
+  "toph2.jpg",
+  "yue1.webp",
+  "zuko1.webp",
+  "zuko2.webp",
+];
+
+function shuffleCards(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function pairUpCards(numberOfPairs) {
+  if (images.length < numberOfPairs) {
+    const numToAdd = numberOfPairs - images.length;
+    const cardsToAdd = images.slice(0, numToAdd);
+    images = [...images, ...cardsToAdd];
+    console.log(images.length, numberOfPairs);
+  }
+  const shuffledImages = shuffleCards(images);
+  const cardsToPair = shuffledImages.slice(0, numberOfPairs);
+  const pairedCards = [...cardsToPair, ...cardsToPair];
+
+  return shuffleCards(pairedCards);
+}
+
+function setUpBoard(boardSize = 4) {
   const numberOfCopies = boardSize ** 2;
   const cardEl = document.querySelector(".card");
   const boardEl = document.querySelector(".board");
 
-  cardEl.style.backgroundImage = "url('assets/aang1.webp')";
+  const numberOfPairs = numberOfCopies / 2;
+  const imagePairs = pairUpCards(numberOfPairs);
+
   boardEl.innerHTML = "";
 
   if (boardSize > 8) {
@@ -12,8 +66,9 @@ function cloneCard(boardSize = 4) {
   }
 
   for (let i = 0; i < numberOfCopies; i++) {
-    const clonedElement = cardEl.cloneNode(true);
-    boardEl.appendChild(clonedElement);
+    const clonedCard = cardEl.cloneNode(true);
+    clonedCard.style.backgroundImage = `url("assets/${imagePairs[i]}")`;
+    boardEl.appendChild(clonedCard);
   }
 
   boardEl.style["grid-template-columns"] = `repeat(${boardSize}, 1fr)`;
@@ -60,7 +115,7 @@ function toggleGame() {
   }
 }
 
-cloneCard(); // 4, 6, 8, 10
+setUpBoard(); // 4, 6, 8, 10
 
 document
   .querySelector(".expand-collapse")
@@ -71,5 +126,5 @@ document.querySelector(".game-toggle").addEventListener("click", toggleGame);
 document
   .querySelector("#game-board-size")
   .addEventListener("change", (event) => {
-    cloneCard(event.target.value);
+    setUpBoard(event.target.value);
   });
