ceb5bcacd20831e1e983d309b97b4fb2a52aacfb
srsexton94 <srsexton94@gmail.com>
2025-02-16 11:32:39 -0500

    add alt text dynamically to images

--- app.js
+++ app.js
@@ -1,31 +1,112 @@
 let images = [
-  "aang1.webp",
-  "aang2.jpg",
-  "aang3.jpg",
-  "appa1.webp",
-  "azula1.jpeg",
-  "cabbages1.avif",
-  "elements1.jpg",
-  "flying1.webp",
-  "gang1.jpg",
-  "gang2.webp",
-  "gyatso.webp",
-  "hama1.webp",
-  "iroh1.jpeg",
-  "iroh2.webp",
-  "iroh3.png",
-  "jeongjeong1.webp",
-  "katara1.jpg",
-  "katara2.webp",
-  "momo1.webp",
-  "ozai1.webp",
-  "sokka1.webp",
-  "sokka2.png",
-  "toph1.jpg",
-  "toph2.jpg",
-  "yue1.webp",
-  "zuko1.webp",
-  "zuko2.webp",
+  {
+    file: "aang1.webp",
+    alt: "Aang softly smiling looking at the sky",
+  },
+  {
+    file: "aang2.jpg",
+    alt: "Aang in disguise with Appa's fur on his head and face",
+  },
+  {
+    file: "aang3.jpg",
+    alt: "Aang with hair in a fire nation disguise",
+  },
+  {
+    file: "appa1.webp",
+    alt: "Appa flying in clear skies",
+  },
+  {
+    file: "azula1.jpeg",
+    alt: "Azula scowling and looking forward",
+  },
+  {
+    file: "cabbages1.avif",
+    alt: "A man in a green turban caressing a cabbage",
+  },
+  {
+    file: "elements1.jpg",
+    alt: "The air, water, fire, and earth logos",
+  },
+  {
+    file: "flying1.webp",
+    alt: "Two air benders flying with orange gliders",
+  },
+  {
+    file: "gang1.jpg",
+    alt: "A group shot of Katara, Toph, Sokka, and Aang in fire nation disguises",
+  },
+  {
+    file: "gang2.webp",
+    alt: "A group shot of Appa, Momo, Aang, Katara, Sokka, and Toph embracing",
+  },
+  {
+    file: "gyatso1.webp",
+    alt: "Gyatso, an airbender master, looking somberly forward",
+  },
+  {
+    file: "hama1.webp",
+    alt: "Hama, a disheveled water bender, looking menacingly forward",
+  },
+  {
+    file: "iroh1.jpeg",
+    alt: "Uncle Iroh playing a table top game with a steaming pot of tea",
+  },
+  {
+    file: "iroh2.webp",
+    alt: "Uncle Iroh smiling widely and brandishing a playing token",
+  },
+  {
+    file: "iroh3.png",
+    alt: "Uncle Iroh singing quietly beneath a tree with a memorial for his son",
+  },
+  {
+    file: "jeongjeong1.webp",
+    alt: "Jeong Jeong, a man with disheveled white hair and two face scars, scowling",
+  },
+  {
+    file: "katara1.jpg",
+    alt: "Katara in a wide fighting stance bending water all around",
+  },
+  {
+    file: "katara2.webp",
+    alt: "Katara in a fire nation disguise looking forward and smiling softly",
+  },
+  {
+    file: "momo1.webp",
+    alt: "Momo, a large eared furry creature, looking forward with wide eyes",
+  },
+  {
+    file: "ozai1.webp",
+    alt: "Ozai, a fire nation ruler, looking forward and scowling",
+  },
+  {
+    file: "sokka1.webp",
+    alt: "Sokka in a fire nation disguise smiling goofily and leaning in close",
+  },
+  {
+    file: "sokka2.png",
+    alt: "Sokka squatting in a low fighting stance, holding a boomerang",
+  },
+  {
+    file: "toph1.jpg",
+    alt: "Toph standing squarely with hands raised in a earth bending stance",
+  },
+  {
+    file: "toph2.jpg",
+    alt: "Toph holding a large championship belt over her head, looking off in the distance",
+  },
+  {
+    file: "yue1.webp",
+    alt: "Yue, a water tribe princess, looking lovingly forward",
+  },
+  {
+    file: "zuko1.webp",
+    alt: "Zuko, with shaggy ear-length hair, looking forward with a serious expression",
+  },
+  {
+    file: "zuko2.webp",
+    alt: "Zuko, with shaggy ear-length hair and a sword on his back, fire bending a flame",
+  },
 ];
 
 function shuffleCards(array) {
@@ -70,7 +151,8 @@ function setUpBoard(boardSize = 4) {
 
   for (let i = 0; i < numberOfCopies; i++) {
     const clonedCard = cardEl.cloneNode(true);
-    clonedCard.style.backgroundImage = `url("assets/${imagePairs[i]}")`;
+    clonedCard.style.backgroundImage = `url("assets/${imagePairs[i].file}")`;
+    clonedCard.setAttribute("alt", imagePairs[i].alt);
     boardEl.appendChild(clonedCard);
   }
 
